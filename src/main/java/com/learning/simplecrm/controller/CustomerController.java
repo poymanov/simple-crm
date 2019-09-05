175e172a4d8e886e0dd3bd464a08e3224b2f56a9
poymanov <n.poymanov@gmail.com>
2019-09-05 21:59:57 +0300

    Add DAO to customer controller

--- src/main/java/com/learning/simplecrm/controller/CustomerController.java
+++ src/main/java/com/learning/simplecrm/controller/CustomerController.java
@@ -1,15 +1,29 @@
 package com.learning.simplecrm.controller;
 
+import com.learning.simplecrm.dao.CustomerDAO;
+import com.learning.simplecrm.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
 
+    private CustomerDAO customerDAO;
+
+    @Autowired
+    public CustomerController(CustomerDAO customerDAO) {
+        this.customerDAO = customerDAO;
+    }
+
     @RequestMapping("/list")
     public String listCustomers(Model model) {
+        model.addAttribute("customers", customerDAO.getCustomers());
+
         return "list-customers";
     }
 }
