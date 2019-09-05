3f204afb40cc6f3e68c05c3ce29325dacfb080ff
poymanov <n.poymanov@gmail.com>
2019-09-05 21:49:38 +0300

    Add customer DAO

---
+++ src/main/java/com/learning/simplecrm/dao/CustomerDAO.java
@@ -0,0 +1,9 @@
+package com.learning.simplecrm.dao;
+
+import com.learning.simplecrm.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDAO {
+    public List<Customer> getCustomers();
+}
